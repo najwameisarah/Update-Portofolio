5e994ee0e60c86fcf9446f4d9eb103a92981f2ec
Najwa Putri Meisarah <najwameisarah623@gmail.com>
2025-04-21 10:01:45 +0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,4 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.getElementById('nav-links');
 
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
 
 
 window.addEventListener("load", () => {
